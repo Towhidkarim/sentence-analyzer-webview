b5139679e56ae6eb098a1a53ab2f5a681e3f0c79
Towhid Karim <towhidkarim123@gmail.com>
2026-01-09 16:49:10 +0600

    ...

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,12 @@
-"use client";
+'use client';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactNode } from 'react';
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
